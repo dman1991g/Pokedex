4f732aef4374a6c6a925c40edf09f49f6ba8d75d
dman1991g <141966927+dman1991g@users.noreply.github.com>
2024-06-22 00:52:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ async function fetchPokemon() {
 
     try {
         // Fetch basic Pokémon information
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`);
-        if (!response.ok) {
+        const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`);
+        if (!pokemonResponse.ok) {
             throw new Error('Pokémon not found');
         }
-        const pokemon = await response.json();
+        const pokemon = await pokemonResponse.json();
 
         // Display basic information
         const pokemonInfo = `
@@ -19,12 +19,19 @@ async function fetchPokemon() {
             <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
             <p>Height: ${pokemon.height / 10} m</p>
             <p>Weight: ${pokemon.weight / 10} kg</p>
-            <p>Type: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+            <p>Type(s): ${pokemon.types.map(type => type.type.name).join(', ')}</p>
         `;
         resultDiv.innerHTML = pokemonInfo;
 
+        // Fetch moves
+        const movesResponse = await fetch(pokemon.moves[0].move.url); // Example: Fetching moves from the first move in the list
+        if (!movesResponse.ok) {
+            throw new Error('Moves not found');
+        }
+        const movesData = await movesResponse.json();
+
         // Display moves
-        const movesList = pokemon.moves.map(moveInfo => moveInfo.move.name).join(', ');
+        const movesList = movesData.names.map(move => move.name).join(', ');
         const movesInfo = `
             <h3>Moves</h3>
             <p>${movesList}</p>
